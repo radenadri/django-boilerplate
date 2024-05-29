45fa5f9de7e72d78748876d8ccaf229a3291a84b
Adriana Eka Prayudha <radenadriep@gmail.com>
2024-05-29 12:49:56 +0700

    feat: first commit ⚡️

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from inertia import render
+
+def home(request):
+    return render(request, 'Home', props={
+        'title': 'Django Inertia Vue.js',
+        'description': 'Belajar melakukan instalasi dan setup Django, Inertia dan Vue.js'
+    })
---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue'
+
+module.exports = {
+  plugins: [
+    vue()
+  ],
+  root: resolve('./assets'),
+  base: '/static/',
+  server: {
+    hmr: {
+      host: 'localhost',
+    },
+    watch: {
+      usePolling: true,
+      disableGlobbing: false,
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  build: {
+    target: 'es2015',
+    assetsDir: '',
+    outDir: resolve('./assets/dist'),
+    manifest: "manifest.json",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve('./assets/js/app.js'),
+      },
+    },
+  },
+};
